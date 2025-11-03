7ef64a00386b127b0723cd6382dcd611152759d9
Kam Rodriguez <kamrod1021@gmail.com>
2025-11-02 17:35:51 -0800

    I added the min functions and I am working on the function to calculate the circumference

--- script.js
+++ script.js
@@ -3,9 +3,22 @@
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
 
+console.log(Math.min(0, 10));
+console.log(Math.min(0, -10));
+
+
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
+function calcCircurference(circumference) {
+  const pi = 3.14;
+  console.log(2 * pi * radius);
+
+}
+
+console.log(calcCircurference(5));
+
+
 
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
